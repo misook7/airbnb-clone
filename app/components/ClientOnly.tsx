20b47d62b8fec0c6fa24d4a4ea8e1f8463c92d41
Leah <msohn@777part.com>
2023-07-25 11:44:36 -0700

    create components

---
+++ app/components/ClientOnly.tsx
@@ -0,0 +1,10 @@
+// 'use client';
+
+// import { useEffect, useState } from "react";
+
+// const ClientOnly = () => {
+//   const [hasMoun]
+//   return (  );
+// }
+ 
+// export default ClientOnly;
