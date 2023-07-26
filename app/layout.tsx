5051f7ee756f78e5ed81146aaa7d838d2f52f11f
Misook <misooksohn7@gmail.com>
2023-07-26 02:09:33 -0700

    add button

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Modal />
+        <Modal isOpen title="title"/>
         <Navbar />
         {children}
       </body>
